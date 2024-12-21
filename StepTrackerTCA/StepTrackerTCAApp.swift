1e624909f130500683449321fb214a5f93c0fc92
Sebastian <61020558+SebaKrk@users.noreply.github.com>
2024-12-21 07:38:04 +0100

    Initial Commit

---
+++ StepTrackerTCA/StepTrackerTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerTCAApp.swift
+//  StepTrackerTCA
+//
+//  Created by Sebastian Sciuba on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
