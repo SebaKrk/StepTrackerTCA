8b8a11fa73ed0b3c4bd9fcb679a0b683fc475e6d
Sebastian <61020558+SebaKrk@users.noreply.github.com>
2025-01-03 23:15:48 +0100

    IOS-00005 HealthData IOS-00005B Fetch HealthKit data

---
+++ StepTrackerTCA/Model/HealthData.swift
@@ -0,0 +1,13 @@
+//
+//  HealthData.swift
+//  StepTrackerTCA
+//
+//  Created by Sebastian Sciuba on 03/01/2025.
+//
+
+import Foundation
+
+struct HealthData {
+    let date: Date
+    let value: Double
+}
