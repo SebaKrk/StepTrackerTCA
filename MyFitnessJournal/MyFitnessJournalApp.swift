8542e29e03e1c6a6256299e434d15e9e2e94a055
SebaKrk <s.sciuba@gmail.com>
2025-05-17 13:14:08 +0200

    IOS-00037 MyFitnessJournal App Watch IOS-00037A My Fitness Journal: Track heart rate & workout notes

---
+++ MyFitnessJournal/MyFitnessJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitnessJournalApp.swift
+//  MyFitnessJournal
+//
+//  Created by Sebastian Sciuba on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyFitnessJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
