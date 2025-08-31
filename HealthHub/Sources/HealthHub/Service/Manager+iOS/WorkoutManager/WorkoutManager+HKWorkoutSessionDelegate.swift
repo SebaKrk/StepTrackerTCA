a23cf4ef34270bde5b110c6630b23b65b42b5d8b
SebaKrk <s.sciuba@gmail.com>
2025-08-31 12:14:53 +0200

    IOS-00046 WorkoutMirrorLive IOS-00046B Sandbox

--- HealthHub/Sources/HealthHub/Service/Manager+iOS/WorkoutManager/WorkoutManager+HKWorkoutSessionDelegate.swift
+++ HealthHub/Sources/HealthHub/Service/Manager+iOS/WorkoutManager/WorkoutManager+HKWorkoutSessionDelegate.swift
@@ -21,7 +21,6 @@ extension DefaultWorkoutManager: HKWorkoutSessionDelegate {
         print("💫 Session state changed from \(fromState.rawValue) to \(toState.rawValue)")
         Task { @MainActor in
             self.workoutSessionIsRunning = toState == .running
-//            self.workoutSessionContinuation?.yield(self.workoutSessionIsRunning)
             self.workoutSessionContinuation?.yield(toState)
         }
         
